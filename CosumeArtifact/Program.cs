41efb702b523e872c0858449a2fd62f0714fe462
Wing Chi Lam <wingchijoe@gmail.com>
2024-04-04 23:40:21 -0400

    Add project files.

---
+++ CosumeArtifact/Program.cs
@@ -0,0 +1,15 @@
+﻿using UtilityLibraries;
+
+namespace CosumeArtifact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+  
+            Console.WriteLine(StringLibrary.StartsWithUpper("123"));
+
+        }
+    }
+}
