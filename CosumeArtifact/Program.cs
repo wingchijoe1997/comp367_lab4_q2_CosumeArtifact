721694f5380e2aba7d4f8f5a57d11b93c2116320
Wing Chi Lam <wingchijoe@gmail.com>
2024-04-05 00:22:24 -0400

    update to version 2

--- CosumeArtifact/Program.cs
+++ CosumeArtifact/Program.cs
@@ -8,7 +8,7 @@ namespace CosumeArtifact
         {
             Console.WriteLine("Hello, World!");
   
-            Console.WriteLine(StringLibrary.StartsWithUpper("123"));
+            Console.WriteLine(StringLibrary.StartsWithUpper("Yes"));
 
         }
     }
